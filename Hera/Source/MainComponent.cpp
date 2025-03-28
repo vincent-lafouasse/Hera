db5607a1e2a1b339526fe0ab106637a8b80b228b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-28 21:29:15 +0100

    set noise volume

--- Hera/Source/MainComponent.cpp
+++ Hera/Source/MainComponent.cpp
@@ -28,14 +28,11 @@ void MainComponent::getNextAudioBlock(
     const juce::AudioSourceChannelInfo& bufferToFill) {
     for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels();
          ++channel) {
-        // Get a pointer to the start sample in the buffer for this audio
-        // output channel
         auto* buffer = bufferToFill.buffer->getWritePointer(
             channel, bufferToFill.startSample);
 
-        // Fill the required number of samples with noise between -0.125 and
-        // +0.125
+        constexpr float noise_volume = 0.1f;
         for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
-            buffer[sample] = random.nextFloat() * 0.25f - 0.125f;
+            buffer[sample] = noise_volume * (2 * random.nextFloat() + 1);
     }
 }
