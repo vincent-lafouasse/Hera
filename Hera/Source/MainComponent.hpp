3154bc075acd54d44f89da8d7b34095ea352adb7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-28 20:54:07 +0100

    include JuceHeader

--- Hera/Source/MainComponent.hpp
+++ Hera/Source/MainComponent.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "JuceHeader.h"
+
 //==============================================================================
 class MainContentComponent : public juce::AudioAppComponent {
    public:
