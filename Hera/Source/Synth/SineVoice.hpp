be265647f922133d481fd48bd89f43c3b4bba1d2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-03 03:05:41 +0200

    dummy implementattion of all necessary juce::Synthesiser methods

--- Hera/Source/Synth/SineVoice.hpp
+++ Hera/Source/Synth/SineVoice.hpp
@@ -3,10 +3,19 @@
 #include "JuceHeader.h"
 #include "SineSound.hpp"
 
-class SineVoice final: public juce::SynthesiserVoice {
+class SineVoice final : public juce::SynthesiserVoice {
+    void renderNextBlock(AudioBuffer<float>& outputBuffer,
+                         int startSample,
+                         int numSamples) override {}
+
+    void startNote(int, float, juce::SynthesiserSound*, int) override {}
+    void stopNote(float velocity, bool allowTailOff) override {}
+
+    void pitchWheelMoved(int newPitchWheelValue) override {}
+    void controllerMoved(int controllerNumber,
+                         int newControllerValue) override {}
+
     bool canPlaySound(juce::SynthesiserSound* sound) override {
         return dynamic_cast<SineSound*>(sound) != nullptr;
     }
-
-    void startNote(int, float, juce::SynthesiserSound*, int) override {}
 };
